3ffb9c43a22bac24dd4a308e00d3d65694bff270
gsanchez13 <gisellesanchez@pursuit.org>
2020-08-23 15:22:52 -0400

    creates basic bootstrap landing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { } from 'react';
 import './App.css';
+import { Button } from './styling/styles';
 
-function App() {
+const App = () => {
   return (
-    <React.Fragment>
-      
-    </React.Fragment>
+    <div>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark"></nav>
+      <main role="main">
+        <div className="jumbotron">
+          <div className="container">
+            <h1 className="display-3 animated fadeInDown">Welcome to DeLife. </h1>
+            <h3 className="display-5 font-weight-light"> Delivering life's luxuries, today. </h3>
+          </div>
+          <p className="font-weight-light ml-3">With a focus on same day delivery for boutique fashion, beauty and homegoods, we are a platform where customers, couriers and vendors can deliver life's luxieres when they are needed. No wait, no waste, more life.</p>
+        </div>
+        <div className="container"></div>
+      </main>
+    </div>
   );
 }
 
