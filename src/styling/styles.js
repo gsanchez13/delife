82c1ea5bca4707a9859c64f0d041ebe159a6792b
gsanchez13 <gisellesanchez@pursuit.org>
2020-08-23 15:23:19 -0400

    creates styled button in styling folder

---
+++ src/styling/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Button  = styled.button`
+background: transparent;
+border-raidus: 3px;
+border: 2px solid darkorange;
+color: darkorange;
+margin: 1em;
+padding: 0.25 em 1 em;
+`
