6bf9e270aa49a5b8ba205fc537cdc621bfc54222
gsanchez13 <gisellesanchez@pursuit.org>
2020-08-25 00:13:15 -0400

    aligns header content

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ const App = () => {
       </nav>
       <main role="main">
         <div className="jumbotron bg-warning">
-          <div className="container-md">
-            <h1 className="display-3 animated fadeInDown">Welcome to DeLife. </h1>
+          <div className="container">
+            <h1 className="display-3">Welcome to DeLife. </h1>
             <h4 className="display-5 font-weight-light"> Delivering life's luxuries, today. </h4>
+            <p className="font-weight-light">With a focus on same day delivery for boutique fashion, beauty and homegoods, we are a platform where customers, couriers and vendors can deliver life's luxeries when they are needed. No wait, no waste, more life.</p>
           </div>
-          <p className="font-weight-light mx-4">With a focus on same day delivery for boutique fashion, beauty and homegoods, we are a platform where customers, couriers and vendors can deliver life's luxeries when they are needed. No wait, no waste, more life.</p>
         </div>
         <div className="container mb-5">
           <div className="row">
@@ -29,23 +29,23 @@ const App = () => {
               </p>
               <a className="btn btn-secondary" href="#" role="button">Customer Signup</a>
             </div>
-            <br/>
+            <br />
             <div className="col-md-4 mt-3">
               <h3>Courier Signup</h3>
               <p className="font-weight-light">
-              The courier platform will use geolocation to obtain your current location and suggest jobs that are nearby, notify you when your order is ready for pickup, and update you on information for that given order. Connected to a payment system that deposits your earnings directly into your bank account, it is a seamless, quick way to make daily earnings. Sign up now to apply to become a courier once DeLife 1.0 launches in a metro-area near you.
+                The courier platform will use geolocation to obtain your current location and suggest jobs that are nearby, notify you when your order is ready for pickup, and update you on information for that given order. Connected to a payment system that deposits your earnings directly into your bank account, it is a seamless, quick way to make daily earnings. Sign up now to apply to become a courier once DeLife 1.0 launches in a metro-area near you.
               </p>
               <a className="btn btn-secondary" href="#">Courier Signup</a>
             </div>
             <div className="col-md-4 mt-3">
               <h3>Store Signup</h3>
               <p className="font-weight-light">
-              The store platform will consist of an online marketplace for small businesses to connect to. You will recieve notifications of orders placed, as well as time estimated for courier pickup and successful delivery notification. into addition, the live inventory will help you keep track of what needs to be replenished and an steady overview of your business's sales. Sign up now to become a partner once DeLife 1.0 launches.
+                The store platform will consist of an online marketplace for small businesses to connect to. You will recieve notifications of orders placed, as well as time estimated for courier pickup and successful delivery notification. into addition, the live inventory will help you keep track of what needs to be replenished and an steady overview of your business's sales. Sign up now to become a partner once DeLife 1.0 launches.
               </p>
               <a className="btn btn-secondary" href="#">Store Signup</a>
             </div>
           </div>
-          <hr/>
+          <hr />
         </div>
       </main>
       <footer className="container">
