866d9277fa5d2c6a82e1f32a0b6b4db8185f6f71
gsanchez13 <gisellesanchez@pursuit.org>
2020-09-01 23:54:04 -0400

    creates about developers skeleton

---
+++ src/Components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div>
+            <div className="row">
+                <div className="col-lg-4">
+                    <svg className="bg-placeholder-img rounded-circle"></svg>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default About;
