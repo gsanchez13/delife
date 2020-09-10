a73a3368903a05e39ff40adea85d2456391042fc
gsanchez13 <gisellesanchez@pursuit.org>
2020-09-09 23:58:03 -0400

    modifies class name in placeholder

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const About = () => {
-    return(
-        <div>
+    return (
+        <div className="container marketing">
             <div className="row">
                 <div className="col-lg-4">
-                    <svg className="bg-placeholder-img rounded-circle"></svg>
+                    <svg className="bg-placeholder-img rounded-circle my-auto mx-auto" style={{ "width": "140px", "height": "140px" }}>
+                        <title> Placeholder</title>
+                    </svg>
                 </div>
             </div>
         </div>
