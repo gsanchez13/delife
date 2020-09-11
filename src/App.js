bbe95aeb306730c9ea292b32c6269f11c0d15bea
gsanchez13 <gisellesanchez@pursuit.org>
2020-09-10 23:56:35 -0400

    fixes contact route

--- src/App.js
+++ src/App.js
@@ -12,16 +12,16 @@ const App = () => {
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
             <Link to="/about" className="active mr-3">About Us</Link>
-            <Link to="/contact" className=" active mr-3">Contact Us</Link>
+            <Link to="/contact" className="active mr-3">Contact Us</Link>
           </li>
           <li className="nav-item ml-3">
           </li>
         </ul>
       </nav>
       <Switch>
-        <Route exact path="/" component={Landing}/>
-        <Route exact path="/contact" component={Contact}>Contact Us</Route>
-        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/about" component={About} />
       </Switch>
       <footer className="container">
         © VeloxVida LLC 2020
