1b127c49e64e2ae0fd614914c59c0db2d156650a
gsanchez13 <gisellesanchez@pursuit.org>
2020-10-02 14:58:00 -0400

    fixes footer spacing

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ const App = () => {
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/about" component={About} />
       </Switch>
-      <footer className="text-black my-auto py-2 px-5" style={{ "backgroundColor": "#a9a9a9" }}>
-        <div className="d-flex bd-highlight mb-3 px-2">
+      <footer className="text-black my-auto py-1 px-5" style={{ "backgroundColor": "#a9a9a9" }}>
+        <div className="d-flex bd-highlight mb-3 px-3">
           <Link to="/about" className="mr-3 no-decoration text-dark p-2 bd-highlight">About Us</Link>
           <Link to="/contact" className="mr-3 no-decoration text-dark p-2 bd-highlight">Contact Us</Link>
           <p className="ml-auto p-2 bd-highlight">
@@ -28,6 +28,7 @@ const App = () => {
             <a href="#" target="_blank" rel="noopener noreferrer"> {" "}
               <AiOutlineTwitter style={{ "color": "black" }} size={20} />
             </a>
+            <br />
             <br />
             © Vida Veloz LLC 2020</p>
         </div>
