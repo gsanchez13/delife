5aef68c9a979f6b7d5bfc7fe658c6863a50d171e
gsanchez13 <gisellesanchez@pursuit.org>
2020-10-01 20:03:03 -0400

    italicizes caption

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -5,10 +5,10 @@ const Landing = () => {
     return (
         <div>
             <main role="main">
-                <div className="jumbotron" style={{"backgroundColor": "#F29900", "color": "white"}}>
+                <div className="jumbotron" style={{ "backgroundColor": "#F29900", "color": "white" }}>
                     <div className="container">
                         <h1 className="display-3 text-center">Welcome to Vida Veloz. </h1>
-                        <h4 className="display-5 font-weight-light text-center"> Shop and you shall recieve. </h4>
+                        <h4 className="display-5 font-weight-light text-center"><em>Shop and you shall recieve.</em></h4>
                     </div>
                 </div>
                 <div className="container mb-5">
