57fe2898395ac0bf0f4bbb01ed72d63ce25e2292
gsanchez13 <gisellesanchez@pursuit.org>
2020-10-02 14:52:04 -0400

    adjusts color

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route, Link } from 'react-router-dom';
 import Landing from './Components/Landing';
 import About from './Components/About';
 import Contact from './Components/Contact';
-import { AiOutlineInstagram, AiOutlineLinkedin, AiOutlineTwitter } from 'react-icons/ai';
+import { AiFillInstagram, AiFillLinkedin, AiOutlineTwitter } from 'react-icons/ai';
 
 const App = () => {
   return (
@@ -13,22 +13,22 @@ const App = () => {
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/about" component={About} />
       </Switch>
-      <footer className="text-white bg-dark my-auto py-2 px-5">
-        <div className="d-flex bd-highlight mb-3">
-          <Link to="/about" className="mr-3 no-decoration text-light p-2 bd-highlight">About Us</Link>
-          <Link to="/contact" className="mr-3 no-decoration text-light p-2 bd-highlight">Contact Us</Link>
+      <footer className="text-black my-auto py-2 px-5" style={{ "backgroundColor": "#a9a9a9" }}>
+        <div className="d-flex bd-highlight mb-3 px-2">
+          <Link to="/about" className="mr-3 no-decoration text-dark p-2 bd-highlight">About Us</Link>
+          <Link to="/contact" className="mr-3 no-decoration text-dark p-2 bd-highlight">Contact Us</Link>
           <p className="ml-auto p-2 bd-highlight">
             Follow Us:
           <a href="#" target="_blank" rel="noopener noreferrer"> {" "}
-              <AiOutlineInstagram style={{ "color": "white" }} size={20}/>
+              <AiFillInstagram style={{ "color": "black" }} size={20} />
             </a>
             <a href="#" target="_blank" rel="noopener noreferrer"> {" "}
-              <AiOutlineLinkedin style={{ "color": "white" }} size={20}/>
+              <AiFillLinkedin style={{ "color": "black" }} size={20} />
             </a>
             <a href="#" target="_blank" rel="noopener noreferrer"> {" "}
-              <AiOutlineTwitter style={{ "color": "white" }} size={20}/>
+              <AiOutlineTwitter style={{ "color": "black" }} size={20} />
             </a>
-            <br/>
+            <br />
             © Vida Veloz LLC 2020</p>
         </div>
       </footer>
