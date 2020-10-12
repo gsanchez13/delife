5a04c383ba0926c8bf84366ee64cfbeeebc7bb8b
gsanchez13 <gisellesanchez@pursuit.org>
2020-10-12 14:45:04 -0400

    changes description into icons

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -13,36 +13,27 @@ const Landing = () => {
                 </div>
                 <div className="container mb-5">
                     <div className="row">
-                        <div className="col-lg-6 mx-auto">
-                            <h3>Order fashion favorite from stores near you.</h3>
-                            <p>Experience online shopping at your door.</p>
+                        <div className="col-lg-6 text-center my-5 mx-auto px-auto pb-6">
+                            <p className="h4 lead">Order fashion favorites from stores near you.<br/>
+                            Experience online shopping at your door.</p>
                         </div>
-                        <div className="col-log-5 mx-auto">
-                        <img src="https://img.icons8.com/wired/64/000000/shopping-bag-full.png" style={{"height": "100px"}}/>
+                        <div className="col-log-4 text-center my-auto mx-auto px-auto py-auto ">
+
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-lg-4 mt-3">
-                            <h3>Customer Signup</h3>
-                            <p className="font-weight-light">
-                                The customer platform will have store pages filled with inventory where you can browse and shop based on store locations near you. Favorites and categories and an explore page will allow you to see all the small shops you never knew you needed. Selected tiers for delivery speed will be available ranging from 3 hour to end-of-day. Sign up here to recieve a notification once VeloxVita 1.0 launches.
-                            </p>
-                            <Link className="btn btn-secondary" to="/auth" role="button">Customer Signup</Link>
+                        <div className="col-lg-4 text-center">
+                        <img src="https://img.icons8.com/dusk/100/000000/shopping-bag.png"/><br/>
+                            <Link className="btn btn-secondary" to="/auth" role="button">Sign up to Shop</Link>
                         </div>
                         <br />
-                        <div className="col-lg-4 mt-3">
-                            <h3>Courier Signup</h3>
-                            <p className="font-weight-light">
-                                The courier platform will use geolocation to obtain your current location and suggest jobs that are nearby, notify you when your order is ready for pickup, and update you on information for that given order. Connected to a payment system that deposits your earnings directly into your bank account, it is a seamless, quick way to make daily earnings. Sign up now to apply to become a courier once VeloxVita 1.0 launches in a metro-area near you.
-                            </p>
-                            <Link className="btn btn-secondary" to="/auth">Courier Signup</Link>
+                        <div className="col-lg-4 text-center">
+                        <img src="https://img.icons8.com/officel/100/000000/scooter.png"/><br/>
+                            <Link className="btn btn-secondary" to="/auth">Signup to be a Courier</Link>
                         </div>
-                        <div className="col-lg-4 mt-3">
-                            <h3>Store Signup</h3>
-                            <p className="font-weight-light">
-                                The store platform will consist of an online marketplace for small businesses to connect to. You will recieve notifications of orders placed, as well as time estimated for courier pickup and successful delivery notification. into addition, the live inventory will help you keep track of what needs to be replenished and an steady overview of your business's sales. Sign up now to become a partner once VeloxVita 1.0 launches.
-                            </p>
-                            <Link className="btn btn-secondary" to="/auth">Store Signup</Link>
+                        <div className="col-lg-4 text-center">
+                            <img src="https://img.icons8.com/clouds/100/000000/online-shop-shopping-bag.png"/><br/>
+                            <Link className="btn btn-secondary" to="/auth">Partner With Us</Link>
                         </div>
                     </div>
                     <hr />
