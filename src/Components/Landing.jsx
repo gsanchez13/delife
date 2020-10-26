02b6a0f245aa9705f8e1dbd756c29b19b33a9843
gsanchez13 <gisellesanchez@pursuit.org>
2020-10-25 23:41:18 -0400

    oves icon over about

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -14,21 +14,21 @@ const Landing = () => {
                     </div>
                 </div>
                 <div className="row mx-auto my-auto">
+                    <div className="col-lg-3 my-5 mx-auto text-center py-2">
+                        <img src="/bags.svg" style={{ "height": "150px" }}></img>
+                    </div>
+                    <div className="col-lg-3 my-auto mx-auto text-center">
+                        <img src="/bike.svg" style={{ "height": "150px" }}></img>
+                    </div>
+                    <div className="col-lg-3 my-auto mx-auto text-center">
+                        <img src="/store.svg" style={{ "height": "150px" }}></img>
+                    </div>
                     <div className="col-lg-6 py-3 px-5 pl-3 text-center" style={{ "backgroundColor": "#C44B3C" }}>
                         <h1 className="display-4 text-light">About <br /><em>Vida Veloz</em></h1>
                     </div>
                     <div className="col-lg-6 py-3 px-5 pl-3" style={{ "backgroundColor": "#EFDDD0" }}>
                         <em>Vida Veloz </em>helps you find and order clothes, beauty products, and everyday essentials near you. <br />
                         How it works: You share your location or type in your zipcode, we tell you the stores near you that deliver to your address. When you find what you're looking for, you can place your order online and we’ll make sure you receive your order by the end of the day.
-                    </div>
-                    <div className="col-lg-3 my-auto mx-auto">
-                        <img src="/bags.svg"></img>
-                    </div>
-                    <div className="col-lg-3 my-auto mx-auto">
-                        <img src="/bike.svg"></img>
-                    </div>
-                    <div className="col-lg-3 my-auto mx-auto">
-                        <img src="/store.svg"></img>
                     </div>
                 </div>
             </main>
