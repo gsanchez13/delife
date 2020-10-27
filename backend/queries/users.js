cdb1934254c2a21add308ff42f5b03d01c8e5044
KathyPuma <kathypuma96@gmail.com>
2020-10-27 12:28:09 -0400

    Update res.json messages for users

--- backend/queries/users.js
+++ backend/queries/users.js
@@ -2,26 +2,42 @@ const db = require('../database/index')
 
 const createUser = async (req, res, next) => {
     try {
-        await db.none(
-            "INSERT INTO users (id, email) VALUES($(id), $(email))",
-            req.body
+        const { id, email } = req.body
+        let user = await db.one(
+            "INSERT INTO users(id, email) VALUES ($1,$2) RETURNING *",
+            [id, email]
         );
+        res.status(200).json({
+            status: "Success",
+            message: "User Created",
+            payload: user
 
-        res.json({ message: "NEW USER CREATED!" })
+        })
     } catch (err) {
-        next(err)
+        res.json({
+            status: "error",
+            message: "User Already Exists"
+        })
+        next(err);
+
     }
 }
 
 const fetchAllUsers = async (req, res, next) => {
     try {
         const users = await db.any('SELECT * FROM users')
-        res.json({
-            users,
-            message: "ALL USERS!"
+        res.status(200).json({
+            status: "Success",
+            message: "All Users",
+            payload: users
+
         })
     } catch (err) {
-        next(err)
+        res.json({
+            status: "error",
+            message: "Failed to retrieve all users"
+        })
+        next(err);
     }
 }
 
