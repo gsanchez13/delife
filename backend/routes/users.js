14ad3ca5d231b5bd057e3933b06b1ab9274e51ba
gsanchez13 <gisellesanchez@pursuit.org>
2020-11-02 23:21:12 -0500

    changes route to adjust to subscribers

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,9 @@
 const users = require('express').Router();
-const { createUser, fetchAllUsers } = require('../queries/users')
+const { createUser, fetchAllSubscribers } = require('../queries/users')
 const { checkFirebaseToken } = require('../middleware/auth')
 
 users.post('/', createUser)
-users.get('/', fetchAllUsers)
+users.get('/', fetchAllSubscribers)
 // users.get('/', checkFirebaseToken, fetchAllUsers)
 
 module.exports = users
