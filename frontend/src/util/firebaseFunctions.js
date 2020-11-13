08c8734bb29485a2a3fc3d60f07c514b3f87d365
KathyPuma <kathypuma96@gmail.com>
2020-11-13 18:24:39 -0500

    Allow user only to signup with Google using firebase

--- frontend/src/util/firebaseFunctions.js
+++ frontend/src/util/firebaseFunctions.js
@@ -9,13 +9,6 @@ export const uiConfig = {
     signInFlow: "popup",
     signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
-    callbacks: {
-        signInSuccessWithAuthResult: () => false
-    }
+    // signInSuccessUrl: '/'
 }
-
